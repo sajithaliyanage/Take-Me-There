1878fd17c133ceb314fdb2b2a546554c6f9eb45a
Sulochana Kodituwakku <sulochana.456@live.com>
2016-10-22 11:05:23 +0530

    DriverVehicleSelection

---
+++ TMT/member_work/sulochana/driver_vehicle_selection/driver_vehicle_selection.java
@@ -0,0 +1,13 @@
+package com.teamwhileloop.my_uis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class driver_vehicle_selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_vehicle_selection);
+    }
+}
