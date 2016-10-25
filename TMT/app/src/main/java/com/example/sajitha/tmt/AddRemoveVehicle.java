b621f738f8570afcf8f9c1539ffcd54db1b95328
Sulochana Kodituwakku <sulochana.456@live.com>
2016-10-25 14:52:47 +0530

    Fixes_1

---
+++ TMT/app/src/main/java/com/example/sajitha/tmt/AddRemoveVehicle.java
@@ -0,0 +1,13 @@
+package com.example.sajitha.tmt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRemoveVehicle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_remove_vehicle);
+    }
+}
