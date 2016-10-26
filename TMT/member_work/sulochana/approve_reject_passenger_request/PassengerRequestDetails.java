247d1daae66b3bd40bc6596d27bed2e2c0e54702
Sulochana Kodituwakku <sulochana.456@live.com>
2016-10-27 00:49:48 +0530

    Passenger request review

---
+++ TMT/member_work/sulochana/approve_reject_passenger_request/PassengerRequestDetails.java
@@ -0,0 +1,13 @@
+package com.teamwhileloop.my_uis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PassengerRequestDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_request_details);
+    }
+}
